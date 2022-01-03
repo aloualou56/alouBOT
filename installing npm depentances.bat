f09fb765637bc64f5bac8c6c11f29307f268904b
Hlias Akritidis <94974426+aloualou56@users.noreply.github.com>
2022-01-03 16:07:05 +0200

    Update installing npm depentances.bat

--- installing npm depentances.bat
+++ installing npm depentances.bat
@@ -1,12 +1,2 @@
 @echo off
-npm install debug
-npm install minecraft-protocol
-npm install protodef
-npm install vec3
-npm install prismarine-world
-npm install prismarine-entity
-npm install prismarine-physics
-npm install mineflayer-pathfinder
-npm install rambda
-npm install worker-thread
-pause
+npm install auto-install debug mineflayer minecraft-protocol protodef vec3 prismarine-world prismarine-entity prismarine-physics mineflayer-pathfinder mineflayer-utils rambda
